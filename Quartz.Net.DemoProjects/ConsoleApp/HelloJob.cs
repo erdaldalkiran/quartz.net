6e1ae2e9e0b882dfbef5098d4c7a86e07a7a02d5
erdalkiran <erdalkiran@gmail.com>
2014-06-27 16:53:42 +0300

    Day 0

---
+++ Quartz.Net.DemoProjects/ConsoleApp/HelloJob.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp
+{
+    using System;
+
+    using Quartz;
+
+    public class HelloJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            var dataMap = context.JobDetail.JobDataMap;
+
+            var value = dataMap.GetString("says");
+            var key = context.JobDetail.Key;
+            Console.WriteLine("Key: {0} Says:{1}",key,value);
+        }
+    }
+}
---
+++ Quartz.Net.DemoProjects/ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp
+{
+    using System;
+    using System.Threading;
+
+    using Common.Logging.Simple;
+
+    using Quartz;
+    using Quartz.Impl;
+
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Common.Logging.LogManager.Adapter = new ConsoleOutLoggerFactoryAdapter{Level = Common.Logging.LogLevel.Info};
+            try
+            {
+                IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+
+                scheduler.Start();
+
+                IJobDetail job = JobBuilder.Create<HelloJob>()
+                    .WithIdentity("jobkey ", "groupkey ")
+                    .UsingJobData("says","Erdal was here!")
+                    .Build();
+
+                ITrigger trigger =
+                    TriggerBuilder.Create()
+                        .WithIdentity("trigger1", "group1")
+                        .StartNow()
+                        .WithSimpleSchedule(x => x.WithIntervalInSeconds(1).RepeatForever())
+                        .Build();
+
+                scheduler.ScheduleJob(job, trigger);
+
+                Thread.Sleep(TimeSpan.FromSeconds(10));
+
+                scheduler.Shutdown();
+            }
+            catch (SchedulerException se)
+            {
+                Console.WriteLine(se);
+            }
+        }
+    }
+}
